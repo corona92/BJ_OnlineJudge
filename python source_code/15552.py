43d2f6b1ec425d3f0227de2b1f28feedd5ac499e
BCNLab <rlaehdgkr7@naver.com>
2019-10-25 16:07:22 +0900

    add 15552.py, 8393.py

---
+++ python source_code/15552.py
@@ -0,0 +1,18 @@
+import sys
+n=input()
+n=int(n)
+a=[]
+sum_a=[]
+for i in range(0,n) :
+    a=sys.stdin.readline().split()
+    print(a)
+# 다시 풀기
+
+#    sum_a.append(sum(a))
+
+#for i in range(0,n) :
+ #   print(sum_a[i])
+
+#a=[sys.stdin.readline() for i in range(n+1)]
+#a=[sys.stdin.readline() for i in range(n+1)]
+#print(a for i in range(0,n+1))
