b224f57cf5402e0bb8b0cc410a202fd961b156de
BCNLab <rlaehdgkr7@naver.com>
2019-10-28 10:25:46 +0900

    add 2439.py

--- python source_code/2439.py
+++ python source_code/2439.py
@@ -1,10 +1,12 @@
 import sys
 a=sys.stdin.readline()
 a=int(a)
-b=" "*(a-1)
-c="*"
+b=(a-1)*2
+p='*'
+pp=''
 for i in range(1,a+1) :
-    print(b+c)
+    pp=p*i
+    print(pp.rjust(b))
 
 
-#다시풀
+#다시풀기
